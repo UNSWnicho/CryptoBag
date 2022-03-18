f74cb1eaa8fb64db7371848b5e2c53fce285f860
UNSWnicho <90664960+UNSWnicho@users.noreply.github.com>
2022-03-18 16:59:56 +1100

    Week 4 tutorial commit

--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/CoinAdapter.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/CoinAdapter.java
@@ -3,18 +3,25 @@ package au.edu.unsw.infs3634.cryptobag;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class CoinAdapter extends RecyclerView.Adapter<CoinAdapter.CoinViewHolder> {
+public class CoinAdapter extends RecyclerView.Adapter<CoinAdapter.CoinViewHolder> implements Filterable {
 
-    private ArrayList<Coin> mCoins;
+    private ArrayList<Coin> mCoins, mCoinsFiltered;
     public CoinAdapter(ArrayList<Coin> coins) {
         mCoins = coins;
+        mCoinsFiltered = coins;
+
     }
 
     @NonNull
@@ -26,18 +33,55 @@ public class CoinAdapter extends RecyclerView.Adapter<CoinAdapter.CoinViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull CoinAdapter.CoinViewHolder holder, int position) {
-        Coin coin = mCoins.get(position);
+        Coin coin = mCoinsFiltered.get(position);
+
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
         holder.name.setText(coin.getName());
+        holder.value.setText(formatter.format(Double.valueOf(coin.getPriceUsd())));
+        holder.change.setText(String.valueOf(coin.getPercentChange1h()) + " %");
+        holder.itemView.setTag(coin.getSymbol());
 
-
-        holder.value.setText(coin.getPriceUsd());
-        holder.change.setText(coin.getPercentChange1h());
 
     }
 
     @Override
     public int getItemCount() {
-        return mCoins.size();
+        return mCoinsFiltered.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    mCoinsFiltered = mCoins;
+                } else {
+                ArrayList<Coin> filteredList = new ArrayList<>();
+                for(Coin coin : mCoins) {
+                    if(coin.getName().toLowerCase().contains(charString.toLowerCase())) {
+                        filteredList.add(coin);
+                    }
+                }
+                    mCoinsFiltered = filteredList;
+                }
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mCoinsFiltered;
+                        return filterResults;
+
+
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mCoinsFiltered = (ArrayList<Coin>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+
+
+
     }
 
     protected static class CoinViewHolder extends RecyclerView.ViewHolder {
@@ -52,5 +96,30 @@ public class CoinAdapter extends RecyclerView.Adapter<CoinAdapter.CoinViewHolder
             change = itemView.findViewById(R.id.tvChange);
 
         }
+//        @Override
+//        public void onClick(View view) { mListener.onClick(view, (String) view.getTag()); }
     }
+
+    // Use the java Comparator Interface and Collections.sort() to sort the list
+    public void sort(final int sortMethod) {
+        if (mCoinsFiltered.size() > 0) {
+            Collections.sort(mCoinsFiltered, new Comparator<Coin>() {
+                @Override
+                public int compare(Coin o1, Coin o2) {
+                    if (sortMethod == 1) {
+                        // Sort the list by name
+                        return o1.getName().compareTo(o2.getName());
+                    } else if(sortMethod == 2) {
+                        // Sort the list by value
+                        return Double.valueOf(o1.getPriceUsd()).compareTo(Double.valueOf(o2.getPriceUsd()));
+                    }
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+
+        }
+        // Notify the adapter on changes to the list data
+        notifyDataSetChanged();
+    }
+    // To-do
 }
--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
@@ -6,8 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "MainActivity";
@@ -34,11 +38,51 @@ public class MainActivity extends AppCompatActivity {
     // Connect the adapter with the RecyclerView
     mRecyclerView.setAdapter(mAdapter);
 
+  }
 
+  // Instantiate the menu
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu, menu);
+    SearchView searchView = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String s) {
+        // Call filterable method from the adapter class to perform filtering
+        mAdapter.getFilter().filter(s);
+        return false;
+      }
 
+      @Override
+      public boolean onQueryTextChange(String s) {
+        mAdapter.getFilter().filter(s);
+        return false;
+      }
+    });
+    return true;
+  }
+
+  // React to user interaction with the menu
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.sortName:
+        // Sort the list by name
+        mAdapter.sort(1);
+        return true;
+      case R.id.sortValue:
+        // Sort the list by PriceUSD
+        mAdapter.sort(2);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
 
   }
-    void launchDetailActivity(String message){
+
+  // Called when user taps on a row on the Recycler View
+    private void launchDetailActivity(String message) {
       Intent intent = new Intent(MainActivity.this, DetailActivity.class);
       intent.putExtra("key", message);
       startActivity(intent);
